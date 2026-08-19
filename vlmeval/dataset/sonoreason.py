b4ad82381d2f48f9d0c1ac5be427fa37a31b0f2a
yasamin-med <yasimedy4724@gmail.com>
2026-08-18 20:49:40 -0400

    adding different prompt strategy

--- vlmeval/dataset/sonoreason.py
+++ vlmeval/dataset/sonoreason.py
@@ -10,6 +10,10 @@ class SonoReasonDD(ImageBaseDataset):
     TYPE = 'VQA'
     DATASET_URL = {'sonoreason_dd_breast': ''}   # local TSV in LMUData
     DATASET_MD5 = {'sonoreason_dd_breast': ''}
+    PROMPT_COLUMNS = {
+        'direct_diagnosis': 'direct_prompt',
+        'reasoning_diagnosis': 'reasoning_prompt',
+    }
 
     def load_data(self, dataset):
         data_root = os.environ.get('LMUData', os.path.expanduser('~/LMUData'))
@@ -22,11 +26,27 @@ class SonoReasonDD(ImageBaseDataset):
                 'to the relative TSV path.'
             )
 
+        prompt_strategy = os.environ.get(
+            'SONOREASON_PROMPT_STRATEGY', 'direct_diagnosis')
+        if prompt_strategy not in self.PROMPT_COLUMNS:
+            choices = ', '.join(sorted(self.PROMPT_COLUMNS))
+            raise ValueError(
+                f'Unsupported SonoReason prompt strategy {prompt_strategy!r}. '
+                f'Choose one of: {choices}.'
+            )
+
         data = pd.read_csv(path, sep='\t')
+        prompt_column = self.PROMPT_COLUMNS[prompt_strategy]
+        required_columns = {'img_data', prompt_column, 'class_label'}
+        missing_columns = required_columns - set(data.columns)
+        if missing_columns:
+            raise ValueError(
+                f'SonoReason TSV is missing required columns: {sorted(missing_columns)}')
+
         # Map the published SonoReason schema to VLMEvalKit's canonical fields.
         data = data.rename(columns={
             'img_data': 'image',
-            'direct_prompt': 'question',
+            prompt_column: 'question',
             'class_label': 'answer',
         })
         data['index'] = range(len(data))
